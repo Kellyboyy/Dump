9b096ff94f1bcdcaf31ab204491d30dec1a760e1
SYED-ZADA <88243531+syedzada1100@users.noreply.github.com>
2022-07-25 16:45:40 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,18 @@
-from Dumpp import _login
-_login()
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Dumpp import _login
+    _login()
+
+elif bit == '32bit':
+    from Dumpp32 import _login
+    _login()
+
+else:
+    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
+    os.system('exit')
